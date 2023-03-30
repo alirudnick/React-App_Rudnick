be1b4c28e7774dff32af705e6192345472dd74c3
Ali Rudnick <ali.rudnick@yahoo.com>
2023-03-30 14:56:35 -0500

    Updates

---
+++ precache-manifest.623fd5970bb44693503e86adb88e3010.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d503ee6e4ec6fda763e",
+    "url": "/React-App_Rudnick/static/css/main.db1e6890.chunk.css"
+  },
+  {
+    "revision": "5d503ee6e4ec6fda763e",
+    "url": "/React-App_Rudnick/static/js/main.89c3fa6d.chunk.js"
+  },
+  {
+    "revision": "e505b9cc8c7d0f749b02",
+    "url": "/React-App_Rudnick/static/js/runtime~main.2507f30d.js"
+  },
+  {
+    "revision": "03652d1f8c6d3943ea33",
+    "url": "/React-App_Rudnick/static/js/2.338e9d12.chunk.js"
+  },
+  {
+    "revision": "676b0382b51e92690926edd481c73985",
+    "url": "/React-App_Rudnick/static/media/DDD.676b0382.JPG"
+  },
+  {
+    "revision": "17539c08b716700f4f38be0607be81b4",
+    "url": "/React-App_Rudnick/static/media/Proj_Longevity.17539c08.png"
+  },
+  {
+    "revision": "2523e37546ef898b8d3970877d3b8bc3",
+    "url": "/React-App_Rudnick/static/media/Proj_Horiseon.2523e375.png"
+  },
+  {
+    "revision": "d09050d200a14abef72da0e7ce1074ae",
+    "url": "/React-App_Rudnick/static/media/Proj_notetaker.d09050d2.png"
+  },
+  {
+    "revision": "7d34457f1668e8341cf04e381c14f2f8",
+    "url": "/React-App_Rudnick/static/media/Proj_Quiz.7d34457f.png"
+  },
+  {
+    "revision": "7d05d2d2acf067c5f89e69e0b981f908",
+    "url": "/React-App_Rudnick/static/media/Proj_password.7d05d2d2.png"
+  },
+  {
+    "revision": "6c10ef36180eb80f07e0bde73bb71427",
+    "url": "/React-App_Rudnick/static/media/Proj_scheduler.6c10ef36.png"
+  },
+  {
+    "revision": "7d8e167cf7b2c4e3dbe5b16961e06de1",
+    "url": "/React-App_Rudnick/static/media/IMG_4455.7d8e167c.jpg"
+  },
+  {
+    "revision": "4b0a6bb98ba996888eaaf80dc41ccc97",
+    "url": "/React-App_Rudnick/index.html"
+  }
+];
